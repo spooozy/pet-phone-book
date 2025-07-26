4ccec721a5135a27c80199c203ee38f31a491f41
Kavinsky228 <ollaurcenco@gmail.com>
2025-07-26 19:16:38 +0300

    Test JS linters (Biome)

---
+++ test.js
@@ -0,0 +1,34 @@
+class NumberProcessor {
+	constructor(numbers) {
+		this.numbers = numbers;
+	}
+
+	getEvenNumbers() {
+		return this.numbers.filter((num) => num % 2 === 0); }
+
+	calculateSum() {
+		return this.numbers.reduce((sum, num) => sum + num, 0);
+
+
+
+	}
+
+
+
+}
+
+function processNumbersDemo() {
+	const numbersArray = [
+		1, 2, 3, 4, 5, 6, 7,
+
+		8, 9, 10,
+	];
+	const processor = new NumberProcessor(numbersArray)
+  ;
+
+	console.log    ("Исходный массив:", numbersArray)     ;
+	console.log("Чётные числа:", processor.getEvenNumbers());
+	console.log("Сумма чисел:", processor.calculateSum());
+}
+
+processNumbersDemo();
