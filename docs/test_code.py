cd66d9ef63c96871284d4cbe4b1182c4da9d961c
Kavinsky228 <ollaurcenco@gmail.com>
2025-07-26 17:11:16 +0300

    Add flake8 config and test code py

---
+++ docs/test_code.py
@@ -0,0 +1,16 @@
+def process_list(  items  ):
+ items.append('новый элемент');return len(items)  # Нарушение: неправильные отступы, лишние пробелы, точка с запятой
+
+my_list = [1,2,3,"текст"] 
+
+print( "Исходный список:",my_list ) 
+
+list_length=process_list(my_list) 
+
+print("Измененный список:",my_list);print("Длина списка:",list_length)
+
+my_list2=[1,2,3,"текст"] 
+my_list3 = [1, 2, 3, "текст"]
+my_list4 = [1, 2, 3, "текст"]
+
+# лялялял  
