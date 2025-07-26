bc8f07963bd9dd2b17466ac0ebcf7f87fad94698
Kavinsky228 <ollaurcenco@gmail.com>
2025-07-26 19:22:23 +0300

    Fix code python

--- test_code.py
+++ test_code.py
@@ -1,15 +1,14 @@
-def process_list(  items  ):
- items.append('новый элемент');return len(items)  # Нарушение: неправильные отступы, лишние пробелы, точка с запятой
+def process_list(  items  ): items.append('новый элемент');return len(items) 
 
 my_list = [1,2,3,"текст"] 
 
-print( "Исходный список:",my_list ) 
+print("Исходный список:",my_list ) 
 
 list_length=process_list(my_list) 
 
 print("Измененный список:",my_list);print("Длина списка:",list_length)
 
-my_list2=[1,2,3,"текст"] 
+my_list2 = [1,2,3,"текст"] 
 my_list3 = [1, 2, 3, "текст"]
 my_list4 = [1, 2, 3, "текст"]
 
